81ec80c9a66215e44a58397c13dd06287e0793fd
surister <surister98@gmail.com>
2019-03-07 16:21:44 +0100

    More utilities for everyone

--- apexpy/utils.py
+++ apexpy/utils.py
@@ -1,18 +1,32 @@
 from typing import NamedTuple
-import os
+from os import environ
+
+from apexpy.exceptions import UnauthorizedError, NotFoundError, PlayerNotFoundError, ServerError
 
 
 class Constants(NamedTuple):
     try:
         from dotenv import load_dotenv
         load_dotenv()
-        try:
-            API_KEY = os.environ['API_KEY']
-        except KeyError:
-            API_KEY = None
+
     except ModuleNotFoundError:
+        pass
+
+    try:
+        API_KEY = environ['APEX_KEY']
+    except KeyError:
         API_KEY = None
 
     BASE_URL = 'https://public-api.tracker.gg/apex/v1/standard/profile/'
 
+    API_OK = 200
+
+    API_ERROR_MAP = {
+
+        401: UnauthorizedError,
+        400: NotFoundError,
+        403: UnauthorizedError,
+        404: PlayerNotFoundError,
+        500: ServerError,
+    }
 
